5f20001787bf81ddad2bbdd494cb952151c1c7b5
jmm <jonmarcos1@gmail.com>
2025-04-29 13:10:32 +0200

    test

--- mlflowMain.py
+++ mlflowMain.py
@@ -101,7 +101,6 @@ model = SimpleNN()
 optimizer = optim.Adam(model.parameters(), lr=lr)
 criterion = nn.CrossEntropyLoss()
 
-#test
 
 # MLflow config
 mlflow.set_tracking_uri("http://167.99.84.228:5000")
